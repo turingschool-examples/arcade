6e031f30ec4faf32c6528e72013b0d8cc0999d35
Dione Wilson <dionew1@gmail.com>
2019-04-18 01:49:57 -0600

    Create route complete

--- routes/api/v1/games.js
+++ routes/api/v1/games.js
@@ -32,4 +32,22 @@ router.get("/:id", function (req, res, next) {
     });
 });
 
+/*POST new game*/
+router.post("/", function (req, res, next) {
+  Game.create({
+    title: req.body.title,
+    price: req.body.price,
+    releaseYear: req.body.releaseYear,
+    active: req.body.active
+  })
+    .then(game => {
+      res.setHeader("Content-Type", "application/json");
+      res.status(201).send(JSON.stringify(game));
+    })
+    .catch(error => {
+      res.setHeader("Content-Type", "application/json");
+      res.status(500).send({ error });
+    });
+});
+
 module.exports = router;
