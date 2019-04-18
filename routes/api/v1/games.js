d9634b99dd89160dafef4145b6af950fa59dab49
Dione Wilson <dionew1@gmail.com>
2019-04-18 01:34:47 -0600

    Complete read routes

---
+++ routes/api/v1/games.js
@@ -0,0 +1,35 @@
+var express = require("express");
+var router = express.Router();
+var Game = require('../../../models').Game;
+
+/* GET all games */
+router.get("/", function (req, res, next) {
+  Game.findAll()
+    .then(games => {
+      res.setHeader("Content-Type", "application/json");
+      res.status(200).send(JSON.stringify(games));
+    })
+    .catch(error => {
+      res.setHeader("Content-Type", "application/json");
+      res.status(500).send({ error })
+    });
+});
+
+/* GET single game */
+router.get("/:id", function (req, res, next) {
+  Games.findAll({
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(game => {
+      res.setHeader("Content-Type", "application/json");
+      res.status(200).send(JSON.stringify(game));
+    })
+    .catch(error => {
+      res.setHeader("Content-Type", "application/json");
+      res.status(500).send({ error })
+    });
+});
+
+module.exports = router;
